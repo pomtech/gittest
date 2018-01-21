2c3265795b886a01dfc5601b3b32a511e358e474
pomtech <pomtech0410@gmail.com>
2018-01-22 00:25:01 +0900

    Update pombit.js

--- javascripts/pombit.js
+++ javascripts/pombit.js
@@ -1,1 +1,8 @@
-alert("야호");
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        if($(".t2 a").hasClass("on") == true){
+            $(".btnB .ty04 a")[0].click(); // 매수
+        }
+    }
+});
